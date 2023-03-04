81779b34294d0d8f0106068692af0e1a6877c70c
Nuseyba <n.kenadiid@gmail.com>
2023-03-04 23:05:11 +0000

    compleed thought models

---
+++ models/Thought.js
@@ -0,0 +1,27 @@
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+          type: String,
+          required: true,
+          maxlength: 280,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: formatDate,
+        },
+        username: {
+          type: String,
+          required: true,
+        },
+        reactions: [reactionSchema],
+      },
+      {
+        toJSON: {
+          virtuals: true,
+          getters: true,
+        },
+        id: false,
+      }
+    );
